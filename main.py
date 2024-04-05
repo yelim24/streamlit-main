e8a0d63951d594ef2f0ec330995d4320d409ebe5
yelim.kim <yelimkim@hufs.ac.kr>
2024-04-05 17:17:12 +0900

    pages test

--- main.py
+++ main.py
@@ -3,16 +3,23 @@ from st_pages import Page, Section, show_pages, add_page_title
 
 st.set_page_config(page_title = "yelim", page_icon="🍀", layout = "wide", initial_sidebar_state = "expanded")
 
+st.sidebar.title("Sophisticated Palette 🎨")
+
 add_page_title()
 
 show_pages(
     [
         Page("pages/yelim.py", "Main", "🙋‍♀️"),
-        Section(name = "Turingbio", icon = "🧬"),
+    ]
+)
+
+st.sidebar.markdown("---")
+show_pages(
+    [
         Page("pages/ontology.py", "Ontology"),
         Page("pages/chatbot.py", "Chatbot"),
         Page("pages/emotion_classification.py"),
         Page("pages/zero_shot_classification.py")
     ]
 )
-
+st.sidebar.markdown("---")
