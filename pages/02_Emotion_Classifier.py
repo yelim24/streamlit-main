1908b32662f4adef7b6c3167ee157fd7bc0ff55f
yelim.kim <yelimkim@hufs.ac.kr>
2024-04-08 14:21:13 +0900

    classification to classifier

--- pages/02_Emotion_Classifier.py
+++ pages/02_Emotion_Classifier.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 st.set_page_config(page_title = "Emotion", layout = "wide", initial_sidebar_state = "expanded")
 
-st.markdown("# Emotion classification")
+st.markdown("# Emotion classifier")
 
-st.sidebar.title("Emotion classification")
+st.sidebar.title("Emotion classifier")
 st.sidebar.markdown("어쩌구 저쩌구 페이지 설명")
 
 st.sidebar.markdown("---")
--- pages/03_Zero_Shot_Classifier.py
+++ pages/03_Zero_Shot_Classifier.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 st.set_page_config(page_title = "ZSC", layout = "wide", initial_sidebar_state = "expanded")
 
-st.markdown("# Zero shot classification")
+st.markdown("# Zero shot classifier")
 
-st.sidebar.title("Zero shot classification")
+st.sidebar.title("Zero shot classifier")
 st.sidebar.markdown("어쩌구 저쩌구 페이지 설명")
 
 st.sidebar.markdown("---")
