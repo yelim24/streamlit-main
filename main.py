946076f59e4933b4ee86cda17fef7d177a2ffa50
yelim.kim <yelimkim@hufs.ac.kr>
2024-04-08 10:26:00 +0900

    sidebar title test

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ from st_pages import Page, Section, show_pages, add_page_title
 
 st.set_page_config(page_title = "yelim", page_icon="🍀", layout = "wide", initial_sidebar_state = "expanded")
 
+st.sidebar.title("웹 타이틀🎨")
+st.sidebar.markdown("어쩌구 저쩌구 웹 설명")
+st.sidebar.caption("Made by [yelim kim](https://github.com/yelim24/streamlit-main)")
+
+st.sidebar.markdown("---")
+
 st.markdown("# Main page")
 st.sidebar.markdown("# Main page")
 # st.sidebar.write("어쩌구 저쩌구 페이지 짧은 설명")
